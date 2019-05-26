d83ac853424fae3eb133807efe35fe432470bf52
felix_bonnier <felix_bonnier@yahoo.com>
2019-05-26 15:06:35 +0200

    adds connect module to App component to be able to pass on the state between components as props

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'semantic-ui-css/semantic.min.css';
+import { Provider } from 'react-redux';
+import configureStore from './state/store/configureStore';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = configureStore();
+window.store = store
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, 
+  document.getElementById('root')
+);
 serviceWorker.unregister();
