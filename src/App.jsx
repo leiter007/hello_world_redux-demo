6563592d337f8808ecf7ef401237ccad7b2e2422
felix_bonnier <felix_bonnier@yahoo.com>
2019-05-26 15:39:30 +0200

    adds input field for user generated greeting, and changes rootReducer to make it possible to change greeting for the proposed greeting.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Header } from 'semantic-ui-react';
+import { Container, Header, Button, Input } from 'semantic-ui-react';
 import { connect } from 'react-redux'
 
 const App = (props) =>{
@@ -7,6 +7,17 @@ const App = (props) =>{
     <>
       <Container>
         <Header as="h1">{props.state.greeting}</Header>
+        <Input
+          placeholder='New greeting...'
+          onBlur={(event) => props.dispatch({ type: 'PROPOSE_GREETING', 
+          greeting: event.target.value })}
+        />
+          <Button
+            pimary
+            onClick={() => props.dispatch({ type: 'CHANGE_GREETING' })}
+          >
+          Change greeting
+          </Button>
       </Container>
     </>
   );
--- src/state/reducers/rootReducer.js
+++ src/state/reducers/rootReducer.js
@@ -1,7 +1,19 @@
 import initialState from '../store/initialState'
 
-const rootReducer = (state = initialState) => {
-  return state
+const rootReducer = (state = initialState, action) => {
+  if (action.type === 'CHANGE_GREETING') {
+    return {
+      ...state,
+      greeting: state.proposed_greeting
+    }
+  } else if (action.type === 'PROPOSE_GREETING') {
+    return {
+      ...state,
+      proposed_greeting: action.greeting
+    }
+  } else {
+    return state
+  }
 }
 
 export default rootReducer
